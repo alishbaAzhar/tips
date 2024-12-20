c8bc3a513ccbda1643bef93d480303358122d867
alishbaAzhar <alishbakandhir58@gmail.com>
2024-12-20 20:16:40 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+function addToCart(femalejacket) {
+    alert(femalejacket  + " has been added to your cart.");
+}
+addToCart(formalshirt){
+
+}alert(formalshirt + "has been added to your cart");
+ addToCart( womenfrock){
+    alert(womenfrock + "has been added to your cart")
+ }
+
+
+
+
+
